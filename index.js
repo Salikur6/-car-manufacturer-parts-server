a455b9553069d52894d7fdcf386ed1031dfd772e
salikur6 <salikur6@gmail.com>
2022-05-26 17:13:27 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use = cors();
+app.use = express.json();
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ddhai.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+        await client.connect();
+        const itemsCollection = client.db('parts_manufacturer').collection('items');
+
+
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Everything is ok')
+});
+
+
+app.listen(port, () => {
+    console.log('Listening port', port);
+});
