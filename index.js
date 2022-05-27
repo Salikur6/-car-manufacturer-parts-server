19ed979b07f7d4940fd3ae4e4aac50ea72cf3206
salikur6 <salikur6@gmail.com>
2022-05-28 00:35:26 +0600

    payment done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const { ObjectID } = require('bson');
 const app = express();
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -22,7 +23,21 @@ async function run() {
         await client.connect();
         const itemsCollection = client.db('parts_manufacturer').collection('items');
         const orderCollection = client.db('parts_manufacturer').collection('order');
+        const paymentCollection = client.db('parts_manufacturer').collection('payment');
 
+
+        //payment intent
+
+        app.post('/create-payment-intent', async (req, res) => {
+            const { totalPrice } = req.body;
+            const amount = totalPrice * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        })
 
         //ItemCollection's items all products get API
         app.get('/items', async (req, res) => {
@@ -72,6 +87,28 @@ async function run() {
             const result = await orderCollection.findOne(query);
             res.send(result);
         })
+
+
+        //orderCollection payment update api
+
+        app.put('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectID(id) };
+            console.log(payment)
+            const updateDoc = {
+                $set: {
+                    paid: true,
+                    transaction: payment.transaction
+                },
+            };
+
+            // const result = await paymentCollection.insertOne(updateDoc)
+            const updatePayment = await orderCollection.updateOne(filter, updateDoc)
+            console.log(updatePayment)
+            res.send(updateDoc);
+        })
+
 
 
     }
